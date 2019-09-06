3e04f527eedfdf5efa8b85d560f748a9e5cca3e3
Roman Yutsis <yucis@author.spb.ru>
2019-09-06 19:21:13 +0300

    First Commit

---
+++ First/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace First
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.moreBtn = new System.Windows.Forms.Button();
+            this.checkBtn = new System.Windows.Forms.Button();
+            this.gameField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // moreBtn
+            // 
+            this.moreBtn.Location = new System.Drawing.Point(12, 358);
+            this.moreBtn.Name = "moreBtn";
+            this.moreBtn.Size = new System.Drawing.Size(75, 23);
+            this.moreBtn.TabIndex = 0;
+            this.moreBtn.Text = "More";
+            this.moreBtn.UseVisualStyleBackColor = true;
+            this.moreBtn.Click += new System.EventHandler(this.MoreBtn_Click);
+            // 
+            // checkBtn
+            // 
+            this.checkBtn.Location = new System.Drawing.Point(350, 358);
+            this.checkBtn.Name = "checkBtn";
+            this.checkBtn.Size = new System.Drawing.Size(75, 23);
+            this.checkBtn.TabIndex = 1;
+            this.checkBtn.Text = "Check";
+            this.checkBtn.UseVisualStyleBackColor = true;
+            this.checkBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // gameField
+            // 
+            this.gameField.Location = new System.Drawing.Point(12, 12);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(100, 22);
+            this.gameField.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 393);
+            this.Controls.Add(this.gameField);
+            this.Controls.Add(this.checkBtn);
+            this.Controls.Add(this.moreBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button moreBtn;
+        private System.Windows.Forms.Button checkBtn;
+        private System.Windows.Forms.TextBox gameField;
+    }
+}
+
---
+++ First/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+
+namespace First
+{
+    public partial class Form1 : Form
+    {   
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameField.Text = "0";
+        }
+        
+        private void MoreBtn_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            var newCard = rnd.Next(0, 11);
+            var deck = int.Parse(gameField.Text.ToString()) + newCard;
+            gameField.Text = deck.ToString();        
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            var finalDeck = int.Parse(gameField.Text.ToString());
+            var computerDeck = computerDeckFinal();
+            if ((finalDeck < 22) && (finalDeck > computerDeck))
+            {
+                gameField.Text = "Yo" + computerDeck.ToString();
+            }
+            else if ( finalDeck > 21)
+            {
+                gameField.Text = "loser";
+            }
+        }
+
+        private int computerDeckFinal()
+        {
+            var startComputerDeck = 0;
+            var rnd = new Random();
+            startComputerDeck = rnd.Next(1, 12) + rnd.Next(1, 12);
+            if (startComputerDeck < 13 )
+            {
+                startComputerDeck += rnd.Next(1, 12);
+            }
+
+            return startComputerDeck;
+        }
+        
+    }
+}
